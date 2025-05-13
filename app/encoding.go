109badaea4edc78d309df7f432cf6fc276711eb9
nutcas3 <mauricee423@gmail.com>
2025-05-13 14:49:53 +0300

    feat: Introduce EncodingConfig for codec management
    This commit introduces an `EncodingConfig` struct to centralize the management of codec-related configurations within the application.
    
    The `EncodingConfig` struct includes fields for the `InterfaceRegistry`, `Marshaler` (Protobuf codec), `TxConfig` (transaction configuration), and the legacy `Amino` codec.
    
    A `MakeEncodingConfig` function is also provided to easily create a default `EncodingConfig` for testing and initialization purposes. This function registers the necessary standard and module interfaces and legacy amino codecs.
    
    This change improves code organization and provides a consistent way to handle encoding configurations throughout the application, supporting both Protobuf and Amino codecs.

---
+++ app/encoding.go
@@ -0,0 +1,38 @@
+package app
+
+import (
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/std"
+	"github.com/cosmos/cosmos-sdk/x/auth/tx"
+)
+
+// EncodingConfig specifies the concrete encoding types to use for the app.
+// This is provided for compatibility between protobuf and amino implementations.
+type EncodingConfig struct {
+	InterfaceRegistry types.InterfaceRegistry
+	Marshaler         codec.Codec
+	TxConfig          client.TxConfig
+	Amino             *codec.LegacyAmino
+}
+
+// MakeEncodingConfig creates an EncodingConfig for testing
+func MakeEncodingConfig() EncodingConfig {
+	amino := codec.NewLegacyAmino()
+	interfaceRegistry := types.NewInterfaceRegistry()
+	marshaler := codec.NewProtoCodec(interfaceRegistry)
+	txCfg := tx.NewTxConfig(marshaler, tx.DefaultSignModes)
+
+	std.RegisterLegacyAminoCodec(amino)
+	std.RegisterInterfaces(interfaceRegistry)
+	ModuleBasics.RegisterLegacyAminoCodec(amino)
+	ModuleBasics.RegisterInterfaces(interfaceRegistry)
+
+	return EncodingConfig{
+		InterfaceRegistry: interfaceRegistry,
+		Marshaler:         marshaler,
+		TxConfig:          txCfg,
+		Amino:             amino,
+	}
+}
